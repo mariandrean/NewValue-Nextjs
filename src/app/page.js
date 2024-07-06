cd7b2fee1ca7a2bede64c254c37788fd759d9c94
mariandrean <m.andreaan@gmail.com>
2024-07-06 11:59:19 +0200

    feat:add-dashboard-link-in-home-page

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/actualidad">Actualidad</Link>
       <Link href="/login">Login</Link>
+      <Link href="/dashboard">Dashboard</Link>
     </main>
   );
 }
