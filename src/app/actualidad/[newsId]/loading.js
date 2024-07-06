f7cdd5ee227b01526d6ff788ad9528796f0af2b6
mariandrean <m.andreaan@gmail.com>
2024-07-06 11:46:00 +0200

    feat:add-loading.js-to-actualidad-and-newsdetails-pages

---
+++ src/app/actualidad/[newsId]/loading.js
@@ -0,0 +1,11 @@
+import LoadingCircle from "@/components/LoadingCircle";
+
+export default function Loading() {
+
+  return (
+    <div role="status" className='flex flex-col place-items-center w-full'>
+      <LoadingCircle />
+      <span>Cargando...</span>
+    </div>
+  )
+}
