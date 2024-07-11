b8b98da1f2217ad1932721b966e4820a0c1231a6
mariandrean <m.andreaan@gmail.com>
2024-07-11 22:04:55 +0200

    feat:add-navbar

--- src/app/actualidad/layout.js
+++ src/app/actualidad/layout.js
@@ -1,10 +1,11 @@
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <>
-
-      <main className="max-w-[1000px] m-5">
+      <Navbar />
+      <main className="mt-14 max-w-[1000px] p-5">
         {children}
       </main>
       <Footer />
