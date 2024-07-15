fb24ffec6b89927a32ff710e57a62372b90c0ed9
mariandrean <m.andreaan@gmail.com>
2024-07-15 20:19:21 +0200

    feat:add-newsform-to-add-page

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,5 +5,8 @@ export const uploadImage = async (e) => {
   data.append("file", e.target.files[0]);
   data.append("upload_preset", "new_value_preset");
   const response = await axios.post(`https://api.cloudinary.com/v1_1/dgi5ujtli/image/upload`, data);
-  return response.data.secure_url;
+  return {
+    url: response.data.secure_url, 
+    name: response.data.original_filename
+  };
 }
